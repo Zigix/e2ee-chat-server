8ad2c7a357a0571eaa8abfcb31d5bd59e99f8771
Zigix <olekwszk7@gmail.com>
2026-06-02 21:06:54 +0200

    Initial dependencies structure

---
+++ src/main/java/org/chatapp/e2eechatserver/E2eeChatServerApplication.java
@@ -0,0 +1,13 @@
+package org.chatapp.e2eechatserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E2eeChatServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(E2eeChatServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/chatapp/e2eechatserver/E2eeChatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chatapp.e2eechatserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E2eeChatServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
